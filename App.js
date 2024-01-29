780a3ad3467c448023550eb37b27900bbb53fa49
Varunjin <157515907+Varunjin@users.noreply.github.com>
2024-01-29 21:46:29 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import WelcomeMessage from "./components/welcome";
+import StudentInfo from "./components/student-info";
+function App() {
+return (
+<div className="App">
+<div class="container-fluid">
+<WelcomeMessage/>
+<StudentInfo/>
+</div>
+</div>
+);
+}
+export default App;
